98a20ec3d4f35898797aa50bee0e770cb226e551
ntorrent5 <nyly05@gmail.com>
2025-06-18 18:17:24 +0200

    rain chart

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -5,6 +5,7 @@ import Title from './components/TitleCard';
 import UVIndex from './components/UVIndexCard';
 import Wind from './components/WindCard';
 import useCurrentWeather from './hooks/useCurrentWeather';
+import WeatherChart from './components/WeatherChart';
 
 function App() {
 
@@ -14,7 +15,7 @@ function App() {
   if(error) return <p>Error: {error}</p>
 
   return (
-    <div class="text-center flex">
+    <div class="text-center flex bg-blue-200">
       <nav class="bg-blue-400 min-h-screen max-w-sm p-3">
         <ul>
           <li>
@@ -22,10 +23,15 @@ function App() {
           </li>
         </ul>
       </nav>
-      <main>
-        <div class="mr-3 grid-rows-2">
+      <main class="w-full flex flex-col min-h-screen justify-between">
+        <div class="mt-3 h-3/6 ">
+          <WeatherChart></WeatherChart>
+        </div>
+        <div class="mr-3 grid grid-cols-3 grid-rows-2 gap-x-4 gap-y-10 p-4">
           <Humidity humidity={data.current.humidity}></Humidity>
           <Wind wind={data.current.wind_mph}></Wind>
+          <FeelsLike sensation_c={data.current.feelslike_c} sensation_f={data.current.feelslike_f}></FeelsLike>
+          <UVIndex uv_index={data.current.uv}></UVIndex>
         </div>
       </main>
       
